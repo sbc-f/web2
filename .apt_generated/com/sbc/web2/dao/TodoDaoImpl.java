bc55008e040fb966242e412a9a340338177850e8
unknown <>
2017-02-28 23:26:25 +0900

    new

--- .apt_generated/com/sbc/web2/dao/TodoDaoImpl.java
+++ .apt_generated/com/sbc/web2/dao/TodoDaoImpl.java
@@ -2,7 +2,7 @@ package com.sbc.web2.dao;
 
 /** */
 @org.springframework.stereotype.Repository()
-@javax.annotation.Generated(value = { "Doma", "2.12.0" }, date = "2017-02-28T22:53:22.061+0900")
+@javax.annotation.Generated(value = { "Doma", "2.12.0" }, date = "2017-02-28T23:22:26.149+0900")
 public class TodoDaoImpl extends org.seasar.doma.internal.jdbc.dao.AbstractDao implements com.sbc.web2.dao.TodoDao {
 
     static {
