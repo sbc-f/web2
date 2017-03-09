6efabaa464bb035f80c2def4759d86b574737dc6
unknown <>
2017-03-09 23:06:23 +0900

    lesson version

--- src/main/java/com/sbc/web2/AppController.java
+++ src/main/java/com/sbc/web2/AppController.java
@@ -1,8 +1,5 @@
 package com.sbc.web2;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -10,9 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.sbc.web2.entity.TodoEntity;
 import com.sbc.web2.form.CalcForm;
-import com.sbc.web2.service.TodoService;
 
 @Controller
 public class AppController {
